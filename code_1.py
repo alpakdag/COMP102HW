1d9098b793763195ffe1183424f0ed48e7bf008a
Alp <akdagalps@gmail.com>
2025-04-13 17:50:29 -1200

    code file was changed

--- code_1.py
+++ code_1.py
@@ -2,3 +2,14 @@ Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (
 Enter "help" below or click "Help" above for more information.
 >>> •	# First line of your first code
 ... •	print("Hello world!")
+
+•	# New line is added
+•	print("Hey! I'm changing the file!")
+
+•	for i in range(5,-1,-1):
+•	    print("--->", i)
+•	    time.sleep(1)
+•	
+•	print("Rocket was launched!")
+
+
